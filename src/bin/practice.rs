a82b1acd5e9266261d163e866df18b81f5a36c12
Nil Goyette <nil.goyette@gmail.com>
2018-03-03 15:25:21 -0500

    Add a practice binary

---
+++ src/bin/practice.rs
@@ -0,0 +1,47 @@
+extern crate docopt;
+extern crate espagnol;
+extern crate rand;
+
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+use docopt::Docopt;
+
+use espagnol::{Language, Questions, get_verb};
+
+static USAGE: &'static str = "
+Pratiquer les verbes français-espagnol à partir des verbes contenus dans un
+fichier texte. Un verbe par ligne, ie. \"manger, comer\"
+
+Usage:
+  practice <verbs> [options]
+  practice (-h | --help)
+  practice (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+";
+
+fn main() {
+    let version = String::from(env!("CARGO_PKG_VERSION"));
+    let args = Docopt::new(USAGE)
+                      .and_then(|dopt| dopt.version(Some(version)).parse())
+                      .unwrap_or_else(|e| e.exit());
+
+    let mut all_verbs = vec![];
+    let reader = BufReader::new(File::open(args.get_str("<verbs>")).unwrap());
+    for line in reader.lines() {
+        let mut line = line.unwrap();
+        if line.starts_with("-") { break; }
+
+        let mut parts = line.split_terminator(", ");
+        let french = get_verb(parts.next().unwrap(), Language::French);
+        let spanish = get_verb(parts.next().unwrap(), Language::Spanish);
+        all_verbs.push((french, spanish));
+    }
+
+    let mut questions = Questions::new(all_verbs, 2);
+    questions.practice();
+}
